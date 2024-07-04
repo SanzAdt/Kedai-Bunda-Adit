a79674e66152739313182baf9079d2567dfa8fc7
Aditya Agustin <adityaagustin197@gmail.com>
2024-07-04 18:28:08 +0700

    menambahkan fiturg

---
+++ src/app.js
@@ -0,0 +1,113 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        items: [
+            { id: 1, name: 'Robusta Brazil', img: '/1.jpg', price: 20000 },
+            { id: 2, name: 'Arabica Blend', img: '2.jpg', price: 25000 },
+            { id: 3, name: 'Primo Passo', img: '3.jpg', price: 30000 },
+            { id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 35000 },
+            { id: 5, name: 'Sumatra Mandheling', img: '5.jpg', price: 40000 },
+        ],
+    }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+            if (!cartItem) {
+                this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+                this.quantity++;
+                this.total += newItem.price;
+            } else {
+                this.items = this.items.map((item) => {
+                    if (item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id) {
+            const cartItem = this.items.find((item) => item.id === id);
+            if (cartItem.quantity > 1) {
+                this.items = this.items.map((item) => {
+                    if (item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                });
+            } else if (cartItem.quantity === 1) {
+                this.items = this.items.filter((item) => item.id != id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+        }
+    });
+});
+
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function() {
+    let allFilled = true;
+    for (let i = 0; i < form.elements.length; i++) {
+        if (form.elements[i].type !== "hidden" && form.elements[i].value.length === 0) {
+            allFilled = false;
+            break;
+        }
+    }
+    checkoutButton.disabled = !allFilled;
+    if (allFilled) {
+        checkoutButton.classList.remove('disabled');
+    } else {
+        checkoutButton.classList.add('disabled');
+    }
+});
+
+checkoutButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objData = Object.fromEntries(data);
+    const message = formatMessage(objData);
+    const apiUrl = 'https://api.whatsapp.com/send';
+    const params = {
+        phone: '62821-1093-6017',
+        text: message
+    };
+    const queryString = new URLSearchParams(params).toString();
+    const url = `${apiUrl}?${queryString}`;
+    window.location.href = url;
+});
+
+const formatMessage = (obj) => {
+    return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+    Data Pesanan
+    ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})`).join('\n')}
+    Total: ${rupiah(obj.total)}
+    Terima Kasih.`;
+}
+
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(number);
+}
