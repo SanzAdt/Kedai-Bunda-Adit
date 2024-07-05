8473b89a0f5038681001d1142aa6c19a71ad7661
Aditya Agustin <adityaagustin197@gmail.com>
2024-07-05 18:57:50 +0700

    membuat website kedai bunda adit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
-            { id: 1, name: 'Robusta Brazil', img: '/1.jpg', price: 20000 },
-            { id: 2, name: 'Arabica Blend', img: '2.jpg', price: 25000 },
-            { id: 3, name: 'Primo Passo', img: '3.jpg', price: 30000 },
-            { id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 35000 },
-            { id: 5, name: 'Sumatra Mandheling', img: '5.jpg', price: 40000 },
+            { id: 1, name: 'Pizza', img: '1.jpg', price: 50000 },
+            { id: 2, name: 'Hotdog', img: '2.jpg', price: 10000 },
+            { id: 3, name: 'Risoles Sosis Mayo', img: '3.jpg', price: 2500  },
+            { id: 4, name: 'Risoles Sayuran', img: '4.png', price: 1500 },
         ],
     }));
 
@@ -77,7 +76,7 @@ form.addEventListener('keyup', function() {
     }
 });
 
-checkoutButton.addEventListener('click', function (e) {
+checkoutButton.addEventListener('click', async function (e) {
     e.preventDefault();
     const formData = new FormData(form);
     const data = new URLSearchParams(formData);
@@ -91,6 +90,28 @@ checkoutButton.addEventListener('click', function (e) {
     const queryString = new URLSearchParams(params).toString();
     const url = `${apiUrl}?${queryString}`;
     window.location.href = url;
+
+    // // Minta transaction token menggunakan AJAX / Fetch
+    // try {
+    //     const response = await fetch('php/placeOrder.php', {
+    //         method: 'POST',
+    //         body: data,
+    //     });
+
+    //     const textResponse = await response.text();
+    //     console.log('Response:', textResponse);
+        
+    //     const result = JSON.parse(textResponse);
+
+    //     if (result.token) {
+    //         console.log(`Order ID: ${result.order_id}`);
+    //         window.snap.pay(result.token);
+    //     } else {
+    //         console.error('Error:', result.error);
+    //     }
+    // } catch(err) {
+    //     console.log(err.message);
+    // }
 });
 
 const formatMessage = (obj) => {
